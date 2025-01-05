2b600fc562a5cc4e865b907d4843712f2163aa6e
sahil <shp2382@g.rit.edu>
2025-01-05 16:24:26 -0500

    needed to add additional settings for CORS to work

--- backend/schuffle/settings.py
+++ backend/schuffle/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'events',
     'tasks',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
