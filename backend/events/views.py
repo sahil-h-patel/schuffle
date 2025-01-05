4fbffb700655d9fe0e027df5537d8b36e6b92d50
sahil <shp2382@g.rit.edu>
2025-01-05 14:25:45 -0500

    base64 encoded json and stored in env

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,22 +1,27 @@
+import base64
 from google_auth_oauthlib.flow import Flow
 from django.conf import settings
 from django.http import JsonResponse, HttpResponse
 from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
 from django.core.cache import cache
+import os
+import json
 
 def google_calendar_callback(request):
     # Retrieve the authorization code from the GET parameters
     code = request.GET.get('code')
-    
+
     if not code:
         return HttpResponse("Authorization code missing!", status=400)
+    
+    client_secrets_json = base64.b64decode(os.getenv("GOOGLE_CLIENT_SECRETS_FILE")).decode("utf-8")
+    client_config = json.loads(client_secrets_json)
 
     try:
         # Initialize the flow again to exchange the code for tokens
-        print(settings.GOOGLE_CLIENT_SECRETS_FILE)
         flow = Flow.from_client_secrets_file(
-            settings.GOOGLE_CLIENT_SECRETS_FILE, 
+            client_config, 
             scopes=['https://www.googleapis.com/auth/calendar'],
             redirect_uri='https://schuffle.up.railway.app/api/auth/callback'
         )
